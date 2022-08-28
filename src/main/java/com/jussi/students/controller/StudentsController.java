d2725703d55642ddeabfea0d74d1144fb79f10b6
Jussi-Savoranta <jussi.savoranta@gmail.com>
2022-08-28 11:36:16 +0300

    add student class, html-file and hello end-point

--- src/main/java/com/jussi/students/controller/StudentsController.java
+++ src/main/java/com/jussi/students/controller/StudentsController.java
@@ -1,5 +1,22 @@
 package com.jussi.students.controller;
 
-public class StudentsController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.jussi.students.domain.Student;
+
+@Controller
+public class StudentsController {
+	
+	Student studentJane = new Student("Jane", "Tarzan");
+	Student studentTarzan = new Student("Me", "Tarzan");
+	Student studentCheeta = new Student("Cheeta", "TheChimp");
+
+	@RequestMapping("/hello")
+	public String printList() {
+		
+		System.out.println(studentJane.toString());
+		
+		return "hello";
+	}
 }
